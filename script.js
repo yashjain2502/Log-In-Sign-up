aff2d6e9d448870c1bb0eb57acb4a0a6ba27eb8f
yashjain2502 <111167850+yashjain2502@users.noreply.github.com>
2023-01-10 13:38:16 +0530

    LOG IN SIGN UP

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+$(".info-item .btn").click(function(){
+    $(".container").toggleClass("log-in");
+});
+
+$(".container-form .btn").click(function(){
+    $(".container").addClass("active");
+});
